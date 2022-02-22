93deb1ae7fb4c0c42de7727ab860af9fb075b37c
ZoeyLewis <89399185+ZoeyLewis@users.noreply.github.com>
2022-02-22 20:43:21 +0000

    Removed some unnecessary files and included a header, bootstrap and Google fonts.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './Fonts.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Dictionary
+      </header>
+      <h2 className="Sub-heading">Discover something new.</h2>
+    </div>
+  );
+}
+
+export default App;
