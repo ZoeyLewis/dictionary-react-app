03cee3c6869f00d8f7b08327f6aa994e33f14eaa
ZoeyLewis <89399185+ZoeyLewis@users.noreply.github.com>
2022-02-27 17:30:26 +0000

    Rendered the results of the search, with each meaning.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css"
 import axios from "axios";
+import Results from "./Results.js"
 
 export default function Dictionary() {
-    let [word, setWord] = useState("")
+    let [word, setWord] = useState("");
+    let [results, setResults] = useState(null);
 
 function handleResponse(response){
-    console.log(response.data[0])
+    setResults(response.data[0]);
 }
 
    function search(event){
        event.preventDefault()
-       alert (`${word}`)
        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
        axios.get(apiUrl).then(handleResponse)
    } 
@@ -19,9 +20,12 @@ function handleWord(event){
 setWord(event.target.value)
 }
     return (
+        <div>
     <form className="SearchForm" onSubmit={search}>
       <input type="search" placeholder="Search for a word" onChange={handleWord}/>
     <button type="submit">Submit</button>
   </form>
-  )
+  <Results results={results}/>
+  </div>
+  );
 }
