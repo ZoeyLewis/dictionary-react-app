0fb2fac3a0dffb35d27a54beac725de51268df73
ZoeyLewis <89399185+ZoeyLewis@users.noreply.github.com>
2022-03-04 21:50:58 +0000

    Reinstalled bootstrap and integrated with Pexel's photo API to return a series of images on search.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import "./Dictionary.css"
 import axios from "axios";
-import Results from "./Results.js"
+import Results from "./Results.js";
+import Photos from "./Photos.js"
 
 export default function Dictionary(props) {
     let [word, setWord] = useState(props.defaultWord);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState (false);
+    let [photos, setPhotos] = useState (null);
 
 function handleResponse(response){
     setResults(response.data[0]);
+}
+function handlePexelsResponse(response){
+    setPhotos(response.data.photos)
 }
 
    function search(){
        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
        axios.get(apiUrl).then(handleResponse)
+
+       let pexelsApiKey = `563492ad6f917000010000019d3eca27583746f8b3d05deab3406298`
+       let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=9`
+       axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}`} }).then(handlePexelsResponse);
    } 
 function handleWord(event){
 setWord(event.target.value)
@@ -38,6 +47,7 @@ if (loaded){
     <button type="submit">Submit</button>
   </form>
   <Results results={results}/>
+  <Photos photos={photos}/>
   </div>
   );}
   else {
